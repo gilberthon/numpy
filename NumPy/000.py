9bd8bcc8f1c11c29de79fc35ee728d71c6aa4582
gilberthon <gilbert.hon@gmail.com>
2019-03-04 14:07:15 +0800

    fix

--- NumPy/000.py
+++ NumPy/000.py
@@ -6,6 +6,6 @@ nsteps = 1000
 draws = np.random.randint(0, 2, size=(nwalks, nsteps))
 steps = np.where(draws > 0, 1, -1)
 walks = steps.cumsum(1)
-hints30 = (np.abs(walks) >= 30).any(1)
-crossing_times = (np.abs(walks[hints30]) >= 30).argmax(1)
+hints = (np.abs(walks) >= 30).any(1)
+crossing_times = (np.abs(walks[hints]) >= 30).argmax(1)
 print(crossing_times.mean())
